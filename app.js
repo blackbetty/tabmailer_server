6fa01a314f43aecc592ad09f98142df87515f7ff
Daniel Golant <dgolant@gmail.com>
2017-07-25 15:50:10 -0600

    testing to see if this drops the hostname in prod

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use('/pages', express.static(__dirname + '/pages'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -98,9 +97,13 @@ if (process.env.NODE_ENV === 'production') {
         if (err) {
             throw err;
         }
-        https.createServer({
+         https.createServer({
             key: keys.serviceKey,
             cert: keys.certificate
-        }, app).listen(process.env.PORT || 9145);
+        }, app).listen(process.env.PORT || 9145, function(){
+            console.log(this.address());
+        });
+        // console.log(https.Server.address);
     });
 }
+

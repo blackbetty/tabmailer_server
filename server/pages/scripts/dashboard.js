741aa573f5290618a3d5dfb1ee91ee6a334bfbfe
Daniel Golant <dgolant@gmail.com>
2018-05-26 10:21:00 -0400

    Fix settings fetch for new auth model

--- server/pages/scripts/dashboard.js
+++ server/pages/scripts/dashboard.js
@@ -2,37 +2,6 @@ var gAuthInstance;
 const FETCH_USER_TABS_URL = '/linksforuser';
 const FETCH_USER_SETTINGS_URL = '/settings';
 
-// runs immediately when gapi loads
-// var loadGapi = function() {
-// 	gapi.load('client:auth2', {
-// 		callback: function() {
-// 			// gapi.auth2.init().then(function() {
-// 			// 	gAuthInstance = gapi.auth2.getAuthInstance();
-
-// 			// 	if (gAuthInstance.isSignedIn.get()) {
-// 			// 		var googleUser = gAuthInstance.currentUser.get();
-// 			// 		dashboardVueInstance.onSignIn(googleUser);
-// 			// 	} else {
-
-// 			// 		gAuthInstance.signIn().then(function(googleUser) {
-// 			// 			dashboardVueInstance.onSignIn(googleUser);
-
-// 			// 		});
-// 			// 	}
-// 			// });
-// 		},
-// 		onerror: function() {
-// 			// Handle loading error.
-// 			alert('gapi.client failed to load!');
-// 		},
-// 		timeout: 5000, // 5 seconds.
-// 		ontimeout: function() {
-// 			// Handle timeout.
-// 			alert('gapi.client could not load in a timely manner!');
-// 		}
-// 	});
-// };
-
 
 // event bus instance
 const EventBus = new Vue();
@@ -44,6 +13,8 @@ var dashboardVueInstance = new Vue({
 		'user-settings': httpVueLoader('/pages/views/dashboard/userSettings.vue'),
 		'user-saved-tabs': httpVueLoader('/pages/views/dashboard/userSavedTabs.vue')
 	},
+	created: function(){
+	},
 	data: function() {
 		return {
 			showSettings: false,
@@ -52,21 +23,30 @@ var dashboardVueInstance = new Vue({
 		};
 	},
 	methods: {
-		sendRequestWithGoogleIDToken: function(method, url, google_id_token, callback) {
+		loadSettings: function () {			
+			this.sendRequest('GET', FETCH_USER_SETTINGS_URL, function (success, res) {
+
+				if (success) {
+
+					EventBus.$emit('settingsLoadedEvent', JSON.parse(res), function () {
+
+						dashboardVueInstance.showSettings = true;
+					});
+				} else {
+
+					// credentialErrorFillerVar Eventually this will have to be an actual functionality
+					var credentialErrorFillerVar = false;
+					if (credentialErrorFillerVar) {
+
+						dashboardVueInstance.credentialError = true;
+					}
+				}
+			});
+		},
+		sendRequest: function(method, url, callback) {
 			var xhr = new XMLHttpRequest();
 			var req_body = {};
-			if (method === 'GET') {
-				var urlWithParams = url + '?google_id_token=' + google_id_token;
-
-				xhr.open(method, urlWithParams, true);
-
-			} else {
-
-				req_body['google_id_token'] = google_id_token;
-
-				xhr.open(method, url, true);
-
-			}
+			xhr.open(method, url, true);
 			xhr.setRequestHeader('Content-Type', 'application/json');
 			xhr.onload = function() {
 				if (this.status === 401 && retry) {
@@ -89,5 +69,7 @@ var dashboardVueInstance = new Vue({
 		}
 
 	},
-	mounted: function() {}
+	mounted: function() {
+		this.loadSettings();
+	}
 });
